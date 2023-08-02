433725ed0e27ea440ed9c084aaa17496805ade2d
alessandro-giuzio <giuzio@icloud.com>
2023-08-02 20:31:08 +0200

    buttons

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('it worked');
+const cookieBox = document.querySelector(".wrapper")
+buttons = cookieBox.querySelectorAll(".button");
+
+const executeCodes = () => {
+  cookieBox.classList.add("show");
+}
+
+// executeCodes function will be called on webpage load;
+window.addEventListener("load", executeCodes)
